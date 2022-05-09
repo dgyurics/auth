fc1fb509d2fc56a8837181362a9609354c206b5e
Dennis Gyurics <dgyurics@gmail.com>
2022-05-08 23:04:42 -0700

    bcrypt added

--- auth.go
+++ auth.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"log"
 	"strconv"
 	"time"
@@ -13,6 +12,7 @@ import (
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -33,13 +33,6 @@ func init() {
 		DB:       0,  // use default DB
 	})
 
-	err = rdb.Set(ctx, "turd", "sandwhich", 0).Err()
-	checkErr(err) // don't crash on err
-
-	val, err := rdb.Get(ctx, "key").Result()
-	checkErr(err) // don't crash on err
-	fmt.Println("key", val)
-
 	connStr := "user=postgres password=postgres host=localhost port=5432 dbname=auth sslmode=disable"
 	pool, err = sql.Open("postgres", connStr) // opens a connection pool, safe for use by multiple goroutines
 	checkErr(err)
@@ -48,17 +41,12 @@ func init() {
 func main() {
 	app := fiber.New()
 
-	app.Get("/", index)
 	app.Post("/register", register)
 	app.Post("/login", login)
 	app.Get("/users", getUsers)
 	app.Get("/health", health)
 
 	app.Listen(":3000")
-}
-
-func index(c *fiber.Ctx) error {
-	return c.SendStatus(404)
 }
 
 func getUsers(c *fiber.Ctx) error {
@@ -82,8 +70,18 @@ func login(c *fiber.Ctx) error {
 	err := c.BodyParser(&user)
 	checkErr(err)
 
-	sqlStatement := `SELECT id, username, password FROM public.user WHERE username = $1 AND password = $2`
-	err = pool.QueryRow(sqlStatement, user.Username, user.Password).Scan(&user.Id, &user.Username, &user.Password)
+	sqlStatement := `SELECT id, username, password FROM public.user WHERE username = $1`
+	var hashedPassword []byte
+	err = pool.QueryRow(sqlStatement, user.Username).Scan(&user.Id, &user.Username, &hashedPassword)
+	if err != nil {
+		handleErr(err, c)
+		return nil
+	}
+	// generate session id
+	// user.Password = nil
+	// store session id in redis, with user account
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(user.Password))
 	if err != nil {
 		handleErr(err, c)
 		return nil
@@ -101,12 +99,21 @@ func register(c *fiber.Ctx) error {
 	err := c.BodyParser(&user)
 	checkErr(err)
 
-	sqlStatement := `INSERT INTO public.user (username, password) VALUES ($1, $2) RETURNING id`
-	err = pool.QueryRow(sqlStatement, user.Username, user.Password).Scan(&user.Id)
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 	if err != nil {
 		handleErr(err, c)
 		return nil
 	}
+
+	sqlStatement := `INSERT INTO public.user (username, password) VALUES ($1, $2) RETURNING id, username`
+	err = pool.QueryRow(sqlStatement, user.Username, hashedPass).Scan(&user.Id, &user.Username)
+	if err != nil {
+		handleErr(err, c)
+		return nil
+	}
+	// generate session id
+	// user.Password = nil
+	// store session id in redis, with user account
 	c.Cookie(&fiber.Cookie{
 		Name:  "userId",
 		Value: strconv.Itoa(user.Id),
@@ -133,14 +140,17 @@ func health(c *fiber.Ctx) error {
 }
 
 func handleErr(err error, c *fiber.Ctx) {
-	if strings.Contains(err.Error(), "pq: duplicate key") {
-		c.Status(409).SendString(err.Error())
-	} else if strings.Contains(err.Error(), "connection refused") {
+	errMsg := err.Error()
+	if strings.Contains(errMsg, "pq: duplicate key") {
+		c.Status(409).SendString(errMsg)
+	} else if strings.Contains(errMsg, "not the hash") {
+		c.Status(401).SendString(errMsg)
+	} else if strings.Contains(errMsg, "connection refused") {
 		c.Status(503).SendString("database unavailable")
 	} else if err == sql.ErrNoRows {
-		c.Status(404).SendString(err.Error())
+		c.Status(404).SendString(errMsg)
 	} else {
-		c.Status(500).SendString(err.Error())
+		c.Status(500).SendString(errMsg)
 	}
 }
 
