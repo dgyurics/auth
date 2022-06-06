767b5c1a5a3526f810f7f622aca573e7f9adf4f8
Dennis Gyurics <dgyurics@gmail.com>
2022-06-05 17:57:36 -0700

    wip authenticate & authorize added

--- auth.go
+++ auth.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"os"
 	"strconv"
 	"strings"
@@ -19,10 +20,8 @@ import (
 )
 
 // tasks
-// add multi role support
 // add registration code option
 // add logging
-// secure get users endpoint
 // create tests for login, register, get users, health
 // deploy via kubernetes
 // switch from fiber to gin
@@ -32,6 +31,10 @@ type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
+
+const ADMIN_ROLE = "admin"
+const USER_ROLE = "user"
+const GUEST_ROLE = "guest"
 
 var ctx = context.Background()
 var pool *sql.DB
@@ -61,8 +64,21 @@ func init() {
 	initTables()
 }
 
+// TODO wrap into single transaction
 func initTables() {
-	_, err := pool.Exec("CREATE TABLE IF NOT EXISTS public.user (id SERIAL PRIMARY KEY, username VARCHAR(100) UNIQUE, password VARCHAR(100) NOT NULL)")
+	_, err := pool.Exec("CREATE TABLE IF NOT EXISTS public.user (" +
+		"id SERIAL PRIMARY KEY," +
+		"username VARCHAR(100) UNIQUE," +
+		"password VARCHAR(100) NOT NULL)")
+	checkErr(err)
+
+	pool.Exec("CREATE TYPE role_enum AS ENUM ('guest', 'user', 'admin')")
+	// checkErr(err) // don't check error for above statement because CREATE TYPE [IF NOT EXISTS] syntax not available
+
+	_, err = pool.Exec("CREATE TABLE IF NOT EXISTS public.role (" +
+		"user_id INTEGER references public.user(id)," +
+		"role role_enum NOT NULL," +
+		"UNIQUE (user_id, role))")
 	checkErr(err)
 }
 
@@ -79,7 +95,43 @@ func main() {
 	app.Listen("127.0.0.1:3000")
 }
 
+// verify cookie exists + session is valid
+func authenticate(c *fiber.Ctx) (string, error) {
+	sessionId := c.Cookies("session", "")
+	if sessionId == "" {
+		return "", errors.New("unauthorized")
+	}
+	// check id exists in redis
+	val, err := rdb.Get(ctx, sessionId).Result()
+	if err != nil {
+		return "", errors.New("unauthorized")
+	}
+	return val, nil
+}
+
+// verify user has the specified role
+func authorize(userId string, role string) error {
+	id, _ := strconv.Atoi(userId)
+	sqlStatement := `SELECT user_id FROM public.role WHERE user_id = $1 AND role = $2`
+	err := pool.QueryRow(sqlStatement, id, role).Scan(&userId)
+	if err != nil {
+		return errors.New("forbidden")
+	}
+	return nil
+}
+
 func getUsers(c *fiber.Ctx) error {
+	// verify user has active session
+	userId, err := authenticate(c)
+	if err != nil {
+		return handleErr(err)
+	}
+	// verify user has admin role
+	err = authorize(userId, ADMIN_ROLE)
+	if err != nil {
+		return handleErr(err)
+	}
+
 	rows, err := pool.Query("SELECT id, username FROM public.user")
 	checkErr(err)
 
@@ -134,8 +186,16 @@ func register(c *fiber.Ctx) error {
 		return handleErr(err)
 	}
 
+	// FIXME wrap inserts into single transaction
+
 	sqlStatement := `INSERT INTO public.user (username, password) VALUES ($1, $2) RETURNING id, username`
 	err = pool.QueryRow(sqlStatement, user.Username, hashedPass).Scan(&user.Id, &user.Username)
+	if err != nil {
+		return handleErr(err)
+	}
+
+	sqlStatement = `INSERT INTO public.role (user_id, role) VALUES ($1, $2)`
+	_, err = pool.Exec(sqlStatement, user.Id, "user")
 	if err != nil {
 		return handleErr(err)
 	}
@@ -178,6 +238,10 @@ func handleErr(err error) error {
 		return fiber.NewError(409)
 	} else if strings.Contains(errMsg, "not the hash") {
 		return fiber.NewError(401, errMsg)
+	} else if strings.Contains(errMsg, "unauthorized") {
+		return fiber.NewError(401, errMsg)
+	} else if strings.Contains(errMsg, "forbidden") {
+		return fiber.NewError(403, errMsg)
 	} else if strings.Contains(errMsg, "connection refused") {
 		return fiber.NewError(503, "error connecting to redis cache")
 	} else if strings.Contains(errMsg, "bad connection") {
