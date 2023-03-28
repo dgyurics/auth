aeff4e224d0e30cc82e732674d4d76ac7bffbbb2
Dennis Gyurics <dgyurics@gmail.com>
2023-03-28 11:04:45 -0700

    generic CORS headers added

--- src/server/http.go
+++ src/server/http.go
@@ -7,8 +7,22 @@ import (
 	"github.com/go-chi/chi/middleware"
 )
 
+func cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")                  // FIXME make configurable
+		w.Header().Set("Allow-Control-Allow-Methods", "GET, POST, OPTIONS") // FIXME make configurable
+		w.Header().Set("Allow-Control-Allow-Headers", "*")                  // FIXME make configurable
+		w.Header().Set("Allow-Control-Allow-Credentials", "true")           // FIXME make configurable
+		if r.Method == "OPTIONS" {
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func initMiddleware(r *chi.Mux) {
 	r.Use(middleware.Logger)
+	r.Use(cors)
 }
 
 func NewHttpServer(addr string) *http.Server {
