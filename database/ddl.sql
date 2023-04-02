a31b88dc440341214a10d34cc0092859ab93fe5f
Dennis Gyurics <dgyurics@gmail.com>
2023-04-01 23:44:10 -0700

    refactor

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,14 +1,14 @@
 -- execute all ddl in auth schema
 CREATE TABLE IF NOT EXISTS "event" (
- "id"         serial PRIMARY KEY not NULL,
- "uuid"       uuid NOT NULL,
- "type"       text NOT NULL,
- "body"       jsonb NOT NULL,
- "created_at" timestamp without time zone default (now() at time zone 'utc')
+  "id"         serial PRIMARY KEY not NULL,
+  "uuid"       uuid NOT NULL,
+  "type"       text NOT NULL,
+  "body"       jsonb NOT NULL,
+  "created_at" timestamp without time zone DEFAULT (now() at time zone 'utc')
 );
 
 CREATE TABLE IF NOT EXISTS "user" (
-  "id"       UUID	PRIMARY KEY,
-  "username" VARCHAR(50) UNIQUE NOT NULL,
-  "password" CHAR(60) NOT NULL -- bcrypt hash
+  "id"       uuid	PRIMARY KEY,
+  "username" varchar(50) UNIQUE NOT NULL,
+  "password" char(60) NOT NULL -- bcrypt hash
 );
