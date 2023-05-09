6533997690c5acf656b31d7e561f965b64e83581
Dennis Gyurics <34282016+dgyurics@users.noreply.github.com>
2023-05-09 01:01:18 -0700

    refactor auth service test

---
+++ auth-server/repository/repotest.go
@@ -0,0 +1,71 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/dgyurics/auth/auth-server/model"
+)
+
+// MockUserRepository is a mock implementation of the UserRepository interface
+type MockUserRepository struct {
+	Users []*model.User
+}
+
+// CreateUser creates a new user
+func (r *MockUserRepository) CreateUser(ctx context.Context, user *model.User) error {
+	err := r.GetUser(ctx, user)
+	if err == nil {
+		return errors.New("user already exists")
+	}
+	r.Users = append(r.Users, user)
+	return nil
+}
+
+// ExistsUser checks if a user exists
+func (r *MockUserRepository) ExistsUser(_ context.Context, username string) bool {
+	for _, u := range r.Users {
+		if u.Username == username {
+			return true
+		}
+	}
+	return false
+}
+
+// GetUser gets a user by username or ID
+func (r *MockUserRepository) GetUser(_ context.Context, user *model.User) error {
+	for _, u := range r.Users {
+		if u.Username == user.Username {
+			user.ID = u.ID
+			user.Password = u.Password
+			return nil
+		}
+		if u.ID == user.ID {
+			user.Username = u.Username
+			user.Password = u.Password
+			return nil
+		}
+	}
+	return errors.New("user not found")
+}
+
+// MockEventRepository is a mock implementation of the EventRepository interface
+type MockEventRepository struct {
+	Events []*model.Event
+}
+
+// CreateEvent creates a new event
+func (r *MockEventRepository) CreateEvent(_ context.Context, event *model.Event) error {
+	r.Events = append(r.Events, event)
+	return nil
+}
+
+// GenerateUniqueUsername generates a unique username for testing
+func GenerateUniqueUsername() string {
+	rand.Seed(time.Now().UnixNano()) // nolint:staticcheck
+	randomSuffix := rand.Intn(100000)
+	return fmt.Sprintf("testuser%d", randomSuffix)
+}
--- auth-server/service/auth_test.go
+++ auth-server/service/auth_test.go
@@ -2,61 +2,61 @@ package service
 
 import (
 	"context"
-	"errors"
 	"testing"
 
 	"github.com/dgyurics/auth/auth-server/model"
+	repo "github.com/dgyurics/auth/auth-server/repository"
 	"github.com/stretchr/testify/require"
 )
 
-// FIXME this can be refactored a lot...
-const test = "test"
+func TestAuthServiceSuite(t *testing.T) {
+	suite := &AuthServiceTestSuite{}
+	suite.Setup()
 
-func TestCreate(t *testing.T) {
-	userRepo := &fakeUserRepository{
-		users: []*model.User{},
+	t.Run("TestCreate", suite.TestCreate)
+	t.Run("TestCreateUserAlreadyExists", suite.TestCreateUserAlreadyExists)
+	t.Run("Login", suite.TestLogin)
+	t.Run("LoginUserNotExist", suite.TestLoginUserNotExist)
+	t.Run("Logout", suite.TestLogout)
+}
+
+type AuthServiceTestSuite struct {
+	userRepo  repo.UserRepository
+	eventRepo repo.EventRepository
+	service   AuthService
+}
+
+func (suite *AuthServiceTestSuite) Setup() {
+	suite.userRepo = &repo.MockUserRepository{
+		Users: []*model.User{},
 	}
-	eventRepo := &fakeEventRepository{
-		events: []*model.Event{},
+	suite.eventRepo = &repo.MockEventRepository{
+		Events: []*model.Event{},
 	}
-	service := NewAuthService(userRepo, eventRepo)
+	suite.service = NewAuthService(suite.userRepo, suite.eventRepo)
+}
 
-	// TODO refactor, use generics?
-	defer userRepo.reset()
-	defer eventRepo.reset()
-
+func (suite *AuthServiceTestSuite) TestCreate(t *testing.T) {
 	user := model.User{
-		Username: test,
-		Password: test,
+		Username: repo.GenerateUniqueUsername(),
+		Password: "test",
 	}
-	err := service.Create(context.Background(), &user)
+	err := suite.service.Create(context.Background(), &user)
 	require.NoError(t, err)
 	// verify user assigned id
 	require.NotEmpty(t, user.ID)
 	// verify user password was hashed
-	require.NotEqual(t, user.Password, test)
+	require.NotEqual(t, user.Password, "test")
 	// verify user.Id is not default uuid
 	require.NotEqual(t, user.ID, "00000000-0000-0000-0000-000000000000")
 	// using authService.Exists verify user was created
-	require.True(t, service.Exists(context.Background(), &user))
+	require.True(t, suite.service.Exists(context.Background(), &user))
 }
 
-func TestCreateUserAlreadyExists(t *testing.T) {
-	userRepo := &fakeUserRepository{
-		users: []*model.User{},
-	}
-	eventRepo := &fakeEventRepository{
-		events: []*model.Event{},
-	}
-	service := NewAuthService(userRepo, eventRepo)
-
-	// TODO refactor, use generics?
-	defer userRepo.reset()
-	defer eventRepo.reset()
-
-	username := test
-	password := test
-	err := service.Create(context.Background(), &model.User{
+func (suite *AuthServiceTestSuite) TestCreateUserAlreadyExists(t *testing.T) {
+	username := repo.GenerateUniqueUsername()
+	password := "pw123"
+	err := suite.service.Create(context.Background(), &model.User{
 		Username: username,
 		Password: password,
 	})
@@ -64,151 +64,47 @@ func TestCreateUserAlreadyExists(t *testing.T) {
 
 	// create user with same username
 	// should return error
-	err = service.Create(context.Background(), &model.User{
+	err = suite.service.Create(context.Background(), &model.User{
 		Username: username,
 		Password: password,
 	})
 	require.Error(t, err)
 }
 
-func TestLogin(t *testing.T) {
-	userRepo := &fakeUserRepository{
-		users: []*model.User{},
-	}
-	eventRepo := &fakeEventRepository{
-		events: []*model.Event{},
-	}
-	service := NewAuthService(userRepo, eventRepo)
-
-	// TODO refactor, use generics?
-	defer userRepo.reset()
-	defer eventRepo.reset()
-
-	username := test
-	password := test
-	err := service.Create(context.Background(), &model.User{
+func (suite *AuthServiceTestSuite) TestLogin(t *testing.T) {
+	username := repo.GenerateUniqueUsername()
+	password := "pw1234"
+	err := suite.service.Create(context.Background(), &model.User{
 		Username: username,
 		Password: password,
 	})
 	require.NoError(t, err)
 
-	err = service.Login(context.Background(), &model.User{
+	err = suite.service.Login(context.Background(), &model.User{
 		Username: username,
 		Password: password,
 	})
 	require.NoError(t, err)
 }
 
-func TestLoginUserNotExist(t *testing.T) {
-	userRepo := &fakeUserRepository{
-		users: []*model.User{},
-	}
-	eventRepo := &fakeEventRepository{
-		events: []*model.Event{},
-	}
-	service := NewAuthService(userRepo, eventRepo)
-
-	// TODO refactor, use generics?
-	defer userRepo.reset()
-	defer eventRepo.reset()
-
-	username := test
-	password := test
-	err := service.Login(context.Background(), &model.User{
+func (suite *AuthServiceTestSuite) TestLoginUserNotExist(t *testing.T) {
+	username := repo.GenerateUniqueUsername()
+	password := "pw123"
+	err := suite.service.Login(context.Background(), &model.User{
 		Username: username,
 		Password: password,
 	})
 	require.Error(t, err)
 }
 
-func TestLogout(t *testing.T) {
-	userRepo := &fakeUserRepository{
-		users: []*model.User{},
-	}
-	eventRepo := &fakeEventRepository{
-		events: []*model.Event{},
-	}
-	service := NewAuthService(userRepo, eventRepo)
-
-	// TODO refactor, use generics?
-	defer userRepo.reset()
-	defer eventRepo.reset()
-
+func (suite *AuthServiceTestSuite) TestLogout(t *testing.T) {
 	user := model.User{
-		Username: test,
-		Password: test,
+		Username: repo.GenerateUniqueUsername(),
+		Password: "test",
 	}
-	err := service.Create(context.Background(), &user)
+	err := suite.service.Create(context.Background(), &user)
 	require.NoError(t, err)
 
-	err = service.Logout(context.Background(), &user)
+	err = suite.service.Logout(context.Background(), &user)
 	require.NoError(t, err)
-}
-
-// TODO move fake repository to own separate file
-
-type fakeUserRepository struct {
-	users []*model.User
-}
-
-func (f *fakeUserRepository) CreateUser(ctx context.Context, user *model.User) error {
-	err := f.GetUser(ctx, user)
-	if err == nil {
-		return errors.New("user already exists")
-	}
-	f.users = append(f.users, user)
-	return nil
-}
-
-func (f *fakeUserRepository) LoginSuccess(_ context.Context, _ *model.User) error {
-	return nil
-}
-
-func (f *fakeUserRepository) GetUser(_ context.Context, user *model.User) error {
-	for _, u := range f.users {
-		if u.Username == user.Username {
-			user.ID = u.ID
-			user.Password = u.Password
-			return nil
-		}
-		if u.ID == user.ID {
-			user.Username = u.Username
-			user.Password = u.Password
-			return nil
-		}
-	}
-	return errors.New("user not found")
-}
-
-func (f *fakeUserRepository) ExistsUser(_ context.Context, username string) bool {
-	for _, u := range f.users {
-		if u.Username == username {
-			return true
-		}
-	}
-	return false
-}
-
-func (f *fakeUserRepository) LogoutUser(ctx context.Context, user *model.User) error {
-	if !f.ExistsUser(ctx, user.Username) {
-		return errors.New("user not found")
-	}
-	return nil
-}
-
-func (f *fakeUserRepository) reset() {
-	f.users = nil
-}
-
-type fakeEventRepository struct {
-	events []*model.Event
-}
-
-func (f *fakeEventRepository) CreateEvent(_ context.Context, event *model.Event) error {
-	f.events = append(f.events, event)
-	return nil
-}
-
-func (f *fakeEventRepository) reset() {
-	f.events = nil
 }
