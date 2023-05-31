8c1f24ef728f005064ee23e1e2e41bd116423b82
Dennis Gyurics <34282016+dgyurics@users.noreply.github.com>
2023-05-30 23:08:56 -0700

    add placeholder for graceful server shutdown/cleanup

--- auth-server/cmd/main.go
+++ auth-server/cmd/main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/dgyurics/auth/auth-server/config"
 	"github.com/dgyurics/auth/auth-server/server"
 )
 
 func main() {
+	// Create new server
 	config := config.New()
+	server := server.NewHTTPServer(":" + config.ServerConfig.Port)
+
+	// Listen for syscall signals for process to interrupt/quit
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig)
+
+	go func() {
+		<-sig
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(30)*time.Second)
+		defer cancel()
+		log.Println("Shutting down server...")
+		err := server.Shutdown(ctx)
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	// Start server
 	log.Println("Auth service listening on port " + config.ServerConfig.Port)
-	srv := server.NewHTTPServer(":" + config.ServerConfig.Port)
-	err := srv.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
+	err := server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Println(err)
 	}
 }
